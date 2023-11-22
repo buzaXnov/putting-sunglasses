3264a1641fe6986b416d461ef4b3713be82a3fcf
Filip Butić <filip1701@gmail.com>
2023-11-23 00:36:52 +0100

    Sunglasses fine edges added, inserted functions and removed unused functions.

--- sunglasses.py
+++ sunglasses.py
@@ -1,49 +1,30 @@
-import os
-
 import cv2
 import numpy as np
 
 from utils import (
     detect_face,
-    draw_landmarks,
-    get_orientation,
     load_assets,
-    rotate_along_axis,
+    blur_face,
+    rotate_sunglasses,
+    add_border_to_image,
+    rescale_to_facesize
 )
 
 
 class PutOn(object):
     def __init__(self):
         self.face_alignment = detect_face()
-        self.sunglasses, self.points = load_assets("sunglasses")
+        self.sunglasses = load_assets("sunglasses")
 
         self.sun_h, self.sun_w, _ = self.sunglasses.shape
-
-    def save_landmarks(self, landmarks):
-        img_copy = self.img.copy()
-        draw_landmarks(img_copy, landmarks)
-
-        landmarks_filename = f"landmarks_{self.filename}"
-        landmarks_filepath = os.path.join(
-            os.getcwd(), "results", landmarks_filename)
-        cv2.imwrite(landmarks_filepath, img_copy)
-
-    def save_result(self):
-        result_filename = f"puton_{self.filename}"
-        result_filepath = os.path.join(os.getcwd(), "results", result_filename)
-        cv2.imwrite(result_filepath, self.img)
 
     def run(self, img):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         all_landmarks, _, face_rectangles = self.face_alignment.get_landmarks_from_image(
             img_rgb, return_bboxes=True)
-        # face_rectangles = self.detector_faces(img_rgb, 0)
 
-        # for i in range(len(face_rectangles)):
         for bbox, landmarks in zip(face_rectangles, all_landmarks):
-            # NOTE: Pazi na float vrijednosti od bounding boxova od lica jer ćeš ih trebati zamutiti ako je sjebana detekcija landmarksa.
-
             left_eye = landmarks[36:42]
             right_eye = landmarks[42:48]
 
@@ -60,68 +41,46 @@ class PutOn(object):
 
             # Check if the circles overlap or cover the whole face
             if distance <= left_eye_radius + right_eye_radius:
-                # If the circles overlap, blur the face
-                face = img[int(bbox[1]):int(bbox[3]),
-                           int(bbox[0]):int(bbox[2])]
-                blurred_face = cv2.GaussianBlur(face, (3, 3), 11)
-                img[int(bbox[1]):int(bbox[3]), int(
-                    bbox[0]):int(bbox[2])] = blurred_face
+                img_rgb = blur_face(img_rgb, bbox)
 
             else:
+                sunglasses = self.sunglasses.copy()
+
                 # nose top, left and right face end points
-                x = int(landmarks[27][0])
-                y = int(landmarks[27][1])
-                x_18 = int(landmarks[17][0])
-                x_27 = int(landmarks[26][0])
+                x = tuple(map(int, landmarks[27]))
+                x_18 = tuple(map(int, landmarks[17]))
+                x_27 = tuple(map(int, landmarks[26]))
 
-                # calculate new width and height, moving distance for adjusting sunglasses
-                width = int(abs(x_18 - x_27))
-                scale = width / self.sun_w
-                height = int(self.sun_h * scale)
+                dx = x_27[0] - x_18[0]
+                dy = x_27[1] - x_18[1]
 
-                move_x = int(self.points[1] * scale)
-                move_y = int(self.points[2] * scale)
+                sunglasses = rescale_to_facesize(sunglasses, dx, dy)
+                sunglasses = add_border_to_image(sunglasses)
+                sunglasses = rotate_sunglasses(sunglasses, dx, dy)
+                height, width, _ = sunglasses.shape
 
-                if width == 0 or height == 0:
-                    continue
-
-                _h, _w, _ = img.shape
-                _, roll, yaw = get_orientation(_w, _h, landmarks)
-                sunglasses = rotate_along_axis(
-                    self.sunglasses, width, height, phi=yaw, gamma=roll)
-
-                # get region of interest on the face to change
-                roi_color = img[(y - move_y):(y + height - move_y),
-                                (x - move_x):(x + width - move_x)]
-
-                # find all non-transparent points
-                index = np.argwhere(sunglasses[:, :, 3] > 0)
-
+                # Overlay the sunglasses on the original image
+                # Get region of interest on the face to change
                 try:
+                    # Get region of interest on the face to change
+                    startY = max(0, x[1]-height//2)
+                    endY = min(img_rgb.shape[0]-1, startY + height)
+                    startX = max(0, x[0]-width//2)
+                    endX = min(img_rgb.shape[1]-1, startX + width)
+
+                    roi_color = img_rgb[startY:endY, startX:endX]
+
+                    # Resize sunglasses to match ROI size
+                    sunglasses = cv2.resize(
+                        sunglasses, (roi_color.shape[1], roi_color.shape[0]), interpolation=cv2.INTER_AREA)
+
+                    # Removing non-transparent points
+                    index = np.argwhere(sunglasses[:, :, 3] > 0)
                     for j in range(3):
                         roi_color[index[:, 0], index[:, 1],
                                   j] = sunglasses[index[:, 0], index[:, 1], j]
-                except Exception as e:
-                    # If the circles overlap, blur the face
-                    face = img[int(bbox[1]):int(bbox[3]),
-                               int(bbox[0]):int(bbox[2])]
-                    blurred_face = cv2.GaussianBlur(face, (3, 3), 11)
-                    img[int(bbox[1]):int(bbox[3]), int(
-                        bbox[0]):int(bbox[2])] = blurred_face
-
-                # set the area of the image of the changed region with sunglasses
-                img[(y - move_y):(y + height - move_y),
-                    (x - move_x):(x + width - move_x)] = roi_color
+                except Exception:
+                    img_rgb = blur_face(img_rgb, bbox)
 
         # self.save_result()
-        return img
-
-
-def main():
-
-    puton = PutOn()
-    puton.images()
-
-
-if __name__ == "__main__":
-    main()
+        return cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
